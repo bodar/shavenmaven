ea704fc2f5baa33563b63b1b74a86cfa595a179e
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-17 19:43:03 +0000

    Work in progress

--- test/com/googlecode/shavenmaven/DependenciesTest.java
+++ test/com/googlecode/shavenmaven/DependenciesTest.java
@@ -5,6 +5,7 @@ import com.googlecode.totallylazy.matchers.NumberMatcher;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static com.googlecode.shavenmaven.Dependencies.load;
 import static com.googlecode.totallylazy.Files.files;
@@ -18,12 +19,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class DependenciesTest {
     @Test
     public void removesAllFilesNotInUrlList() throws Exception{
-        File temporaryFile = temporaryFile();
-        write("http://yatspec.googlecode.com/files/yatspec-87.jar\n".getBytes(), temporaryFile);
-        Dependencies dependencies = load(temporaryFile);
         File temporaryDirectory = temporaryDirectory();
         temporaryFile(temporaryDirectory);
-        dependencies.update(temporaryDirectory);
+        temporaryFile(temporaryDirectory);
+        assertThat(files(temporaryDirectory).size(), NumberMatcher.is(2));
+
+        load(listOfDependenciesInAFile()).update(temporaryDirectory);
+
         Sequence<File> files = files(temporaryDirectory);
         assertThat(files.size(), NumberMatcher.is(1));
         assertThat(files.contains(new File(temporaryDirectory, "yatspec-87.jar")), is(true));
@@ -31,33 +33,32 @@ public class DependenciesTest {
 
     @Test
     public void supportsLoadingFromFile() throws Exception{
-        File temporaryFile = temporaryFile();
-        String fileContents = "http://yatspec.googlecode.com/files/yatspec-87.jar\n" +
-                              "mvn://repo.bodar.com/com.googlecode.yadic:yadic:jar:116\n" +
-                              "mvn:org.objenesis:objenesis:jar:1.2";
-        write(fileContents.getBytes(), temporaryFile);
-        Dependencies dependencies = load(temporaryFile);
         File temporaryDirectory = temporaryDirectory();
-        dependencies.update(temporaryDirectory);
-        Sequence<File> files = sequence(temporaryDirectory.listFiles());
-        assertThat(files.size(), NumberMatcher.is(3));
-        assertThat(files.contains(new File(temporaryDirectory, "yadic-116.jar")), is(true));
-        assertThat(files.contains(new File(temporaryDirectory, "yatspec-87.jar")), is(true));
+
+        load(listOfDependenciesInAFile()).update(temporaryDirectory);
+
+        Sequence<File> files = files(temporaryDirectory);
+        assertThat(files.size(), NumberMatcher.is(1));
+        assertThat(files.contains(new File(temporaryDirectory, "dependency.txt")), is(true));
+    }
+
+    private File listOfDependenciesInAFile() throws IOException {
+        File temporaryFile = temporaryFile();
+        String fileContents = getClass().getResource("dependency.txt").toString() + "\n";
+        write(fileContents.getBytes(), temporaryFile);
+        return temporaryFile;
     }
 
     @Test
     public void supportsIgnoresEmptyLines() throws Exception{
-        File temporaryFile = temporaryFile();
-        String fileContents = "http://yatspec.googlecode.com/files/yatspec-87.jar\n" +
-                              "\n" +
-                              "mvn:org.objenesis:objenesis:jar:1.2\n";
-        write(fileContents.getBytes(), temporaryFile);
-        Dependencies dependencies = load(temporaryFile);
         File temporaryDirectory = temporaryDirectory();
-        dependencies.update(temporaryDirectory);
+        File list = listOfDependenciesInAFile();
+        write("\n".getBytes(), list);
+
+        load(list).update(temporaryDirectory);
+
         Sequence<File> files = sequence(temporaryDirectory.listFiles());
-        assertThat(files.size(), NumberMatcher.is(2));
-        assertThat(files.contains(new File(temporaryDirectory, "yatspec-87.jar")), is(true));
-        assertThat(files.contains(new File(temporaryDirectory, "objenesis-1.2.jar")), is(true));
+        assertThat(files.size(), NumberMatcher.is(1));
+        assertThat(files.contains(new File(temporaryDirectory, "dependency.txt")), is(true));
     }
 }
---
+++ test/com/googlecode/shavenmaven/MvnArtifactTest.java
@@ -0,0 +1,32 @@
+package com.googlecode.shavenmaven;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MvnArtifactTest {
+    @Test
+    public void supportsUriWithNoExplicitRepository() throws Exception {
+        MvnArtifact mvnArtifact = new MvnArtifact(URI.create("mvn:org.objenesis:objenesis:jar:1.2"));
+        assertThat(mvnArtifact.group(), is("org.objenesis"));
+        assertThat(mvnArtifact.id(), is("objenesis"));
+        assertThat(mvnArtifact.type(), is("jar"));
+        assertThat(mvnArtifact.version(), is("1.2"));
+        assertThat(mvnArtifact.url(), is(new URL("http://repo1.maven.org/maven2/org/objenesis/objenesis/1.2/objenesis-1.2.jar")));
+    }
+
+    @Test
+    public void supportsUriWithExplicitRepository() throws Exception {
+        MvnArtifact mvnArtifact = new MvnArtifact(URI.create("mvn://repo.bodar.com/com.googlecode.yadic:yadic:jar:116"));
+        assertThat(mvnArtifact.group(), is("com.googlecode.yadic"));
+        assertThat(mvnArtifact.id(), is("yadic"));
+        assertThat(mvnArtifact.type(), is("jar"));
+        assertThat(mvnArtifact.version(), is("116"));
+        assertThat(mvnArtifact.url(), is(new URL("http://repo.bodar.com/com/googlecode/yadic/yadic/116/yadic-116.jar")));
+    }
+}
