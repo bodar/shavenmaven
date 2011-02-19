176ba4760e9ef3f517bc6fd707c0cbc58271f4b9
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-19 20:03:51 +0000

    Made tests more reliable to run

--- src/com/googlecode/shavenmaven/Resolver.java
+++ src/com/googlecode/shavenmaven/Resolver.java
@@ -4,23 +4,35 @@ import com.googlecode.totallylazy.Callable1;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
-import static com.googlecode.shavenmaven.Artifacts.writeTo;
+import static com.googlecode.totallylazy.Bytes.bytes;
+import static com.googlecode.totallylazy.Files.write;
 import static java.lang.String.format;
 
 public class Resolver {
     private final File directory;
+    private final PrintStream printStream;
 
-    public Resolver(File directory) {
+    public Resolver(File directory, PrintStream printStream) {
         if(!directory.isDirectory()) {
             throw new IllegalArgumentException("'file' argument must be a directory");
         }
         this.directory = directory;
+        this.printStream = printStream;
+    }
+
+    public Resolver(File directory) {
+        this(directory, System.out);
     }
 
     public Resolver resolve(Artifact artifact) throws IOException {
-        System.out.println(format("Downloading %s", artifact));
-        writeTo(artifact, directory);
+        printStream.println(format("Downloading %s", artifact));
+        try {
+            write(bytes(artifact.url().openStream()), new File(directory, artifact.filename()));
+        } catch (IOException e) {
+            printStream.println(format("Failed to download %s (%s)", artifact, e));
+        }
         return this;
     }
 
--- test/com/googlecode/shavenmaven/DependenciesTest.java
+++ test/com/googlecode/shavenmaven/DependenciesTest.java
@@ -2,27 +2,40 @@ package com.googlecode.shavenmaven;
 
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.matchers.NumberMatcher;
+import com.sun.net.httpserver.HttpServer;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import static com.googlecode.shavenmaven.Artifacts.artifact;
 import static com.googlecode.shavenmaven.Dependencies.load;
-import static com.googlecode.totallylazy.Files.files;
-import static com.googlecode.totallylazy.Files.temporaryDirectory;
-import static com.googlecode.totallylazy.Files.temporaryFile;
-import static com.googlecode.totallylazy.Files.write;
+import static com.googlecode.shavenmaven.Http.createHttpsServer;
+import static com.googlecode.shavenmaven.Http.returnResponse;
+import static com.googlecode.shavenmaven.Http.urlOf;
+import static com.googlecode.totallylazy.Files.*;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DependenciesTest {
-    public static final String DEPENDENCY_FILENAME = "dependency.txt";
-    public static final String DEPENDENCY_PATH = "test/com/googlecode/shavenmaven/" + DEPENDENCY_FILENAME;
+    static final String DEPENDENCY_FILENAME = "dependency.txt";
+    private HttpServer server;
+
+    public static Artifact dependencyFrom(HttpServer server) throws MalformedURLException {
+        return artifact(urlOf(server) + DEPENDENCY_FILENAME);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        server = createHttpsServer(returnResponse(200, "Some dependency content"));
+    }
 
     @Test
     public void removesAllFilesNotInUrlList() throws Exception{
+
         File temporaryDirectory = temporaryDirectory();
         temporaryFile(temporaryDirectory);
         temporaryFile(temporaryDirectory);
@@ -50,7 +63,7 @@ public class DependenciesTest {
     public void supportsIgnoresEmptyLines() throws Exception{
         File temporaryDirectory = temporaryDirectory();
         File list = temporaryFile();
-        write((dependencyUrl() + "\n\n\n").getBytes(), list);
+        write((dependencyFrom(server) + "\n\n\n").getBytes(), list);
 
         load(list).update(temporaryDirectory);
 
@@ -59,19 +72,12 @@ public class DependenciesTest {
         assertThat(files.contains(new File(temporaryDirectory, DEPENDENCY_FILENAME)), is(true));
     }
 
-    public static File listOfDependenciesInAFile() throws IOException {
+    private File listOfDependenciesInAFile() throws IOException {
         File temporaryFile = temporaryFile();
-        String fileContents = dependencyUrl() + "\n";
+        String fileContents = dependencyFrom(server) + "\n";
         write(fileContents.getBytes(), temporaryFile);
         return temporaryFile;
     }
 
-    public static String dependencyUrl() {
-        try {
-            return new File(DEPENDENCY_FILENAME).toURL().toString();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 }
