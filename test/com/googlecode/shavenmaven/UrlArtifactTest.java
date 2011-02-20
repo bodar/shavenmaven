0095ad105defc8df5cb8353d833ebbf4acc431d8
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-20 09:37:52 +0000

    Fixed slow tests - URL.equals is evil

--- test/com/googlecode/shavenmaven/UrlArtifactTest.java
+++ test/com/googlecode/shavenmaven/UrlArtifactTest.java
@@ -12,7 +12,7 @@ public class UrlArtifactTest {
     @Test
     public void supportVanillaHttpUrl() throws Exception {
         UrlArtifact artifact = new UrlArtifact("http://yatspec.googlecode.com/files/yatspec-87.jar");
-        assertThat(artifact.url(), is(new URL("http://yatspec.googlecode.com/files/yatspec-87.jar")));
+        assertThat(artifact.url().toString(), is(new URL("http://yatspec.googlecode.com/files/yatspec-87.jar").toString()));
         assertThat(artifact.filename(), is("yatspec-87.jar"));
     }
 }
