5cc6580cfa2cf41e66c7e4a443c3adc02d132569
dan <dan@bodar.com>
2012-11-06 15:48:13 +0000

    Added comment support in dependency file

--- src/com/googlecode/shavenmaven/S3Artifact.java
+++ src/com/googlecode/shavenmaven/S3Artifact.java
@@ -3,11 +3,12 @@ package com.googlecode.shavenmaven;
 import java.util.regex.Pattern;
 
 public class S3Artifact extends DelegatingArtifact {
+    public static String PROTOCOL = "s3";
+    private static Pattern pattern = Pattern.compile(PROTOCOL + "\\:\\/\\/([^\\/]+)/");
+
     protected S3Artifact(Artifact artifact) {
         super(artifact);
     }
-
-    private static Pattern pattern = Pattern.compile("s3\\:\\/\\/([^\\/]+)/");
 
     public static Iterable<MvnArtifact> parse(final String value) {
         return MvnArtifact.parse(pattern.matcher(value).replaceFirst("mvn://$1.s3.amazonaws.com/"));
