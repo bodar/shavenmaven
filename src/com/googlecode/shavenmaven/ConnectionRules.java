2f5728d0a279c77b0639858610d51d8eab631b0d
Daniel Worthington-Bodart <dan@bodar.com>
2013-08-20 10:21:27 +0100

    Started moving to use UtterlyIdle

--- src/com/googlecode/shavenmaven/ConnectionRules.java
+++ src/com/googlecode/shavenmaven/ConnectionRules.java
@@ -3,8 +3,9 @@ package com.googlecode.shavenmaven;
 import com.googlecode.totallylazy.Function1;
 import com.googlecode.totallylazy.Rule;
 import com.googlecode.totallylazy.Rules;
-
-import java.net.URLConnection;
+import com.googlecode.totallylazy.Uri;
+import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.RequestBuilder;
 
 import static com.googlecode.totallylazy.Predicates.always;
 import static com.googlecode.totallylazy.Rule.rule;
@@ -12,22 +13,21 @@ import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class ConnectionRules {
 
-    public static Rules<Artifact, URLConnection> connectByUrlRules() {
-        final Rule<Artifact, URLConnection> rule = alwaysConnectByUrl();
-        return Rules.<Artifact, URLConnection>rules(sequence(rule));
+    public static Rules<Artifact, Request> connectByUrlRules() {
+        final Rule<Artifact, Request> rule = alwaysConnectByUrl();
+        return Rules.<Artifact, Request>rules(sequence(rule));
     }
 
-    public static Rule<Artifact, URLConnection> alwaysConnectByUrl() {
+    public static Rule<Artifact, Request> alwaysConnectByUrl() {
         return rule(always(Artifact.class), connectByUrl());
     }
 
-    private static Function1<Artifact, URLConnection> connectByUrl() {
-        return new Function1<Artifact, URLConnection>() {
+    private static Function1<Artifact, Request> connectByUrl() {
+        return new Function1<Artifact, Request>() {
             @Override
-            public URLConnection call(Artifact artifact) throws Exception {
-                return artifact.url().openConnection();
+            public Request call(Artifact artifact) throws Exception {
+                return RequestBuilder.get(Uri.uri(artifact.url())).build();
             }
         };
     }
-
 }
--- test/com/googlecode/shavenmaven/s3/S3ConnectorTest.java
+++ test/com/googlecode/shavenmaven/s3/S3ConnectorTest.java
@@ -1,12 +1,16 @@
 package com.googlecode.shavenmaven.s3;
 
+import com.googlecode.totallylazy.time.StoppedClock;
+import com.googlecode.utterlyidle.Request;
 import org.junit.Test;
 
 import java.net.HttpURLConnection;
+import java.util.Date;
 
 import static com.googlecode.shavenmaven.Artifacts.artifact;
 import static com.googlecode.shavenmaven.s3.AwsCredentials.awsCredentials;
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.time.Dates.date;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -14,8 +18,9 @@ public class S3ConnectorTest {
 
     @Test
     public void createsAwsAuthorisedHttpConnection() throws Exception {
-        final S3Connector connector = new S3Connector(awsCredentials("*", "access-key", "secret-key"));
-        final HttpURLConnection connection = (HttpURLConnection) connector.call(sequence(artifact("s3://repo.bodar.com/com.googlecode.yadic:yadic:jar:116")).head());
-        assertThat(connection.getURL().toString(), is("http://s3.amazonaws.com:80/repo.bodar.com/com/googlecode/yadic/yadic/116/yadic-116.jar"));
+        Date now = date(2001, 1, 1);
+        S3Connector connector = new S3Connector(awsCredentials("*", "access-key", "secret-key"), new StoppedClock(now));
+        Request request = connector.call(sequence(artifact("s3://repo.bodar.com/com.googlecode.yadic:yadic:jar:116")).head());
+        assertThat(request.uri().toString(), is("http://s3.amazonaws.com/repo.bodar.com/com/googlecode/yadic/yadic/116/yadic-116.jar"));
     }
 }
