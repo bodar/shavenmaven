74768d98b7ca429f65be8d9b7955611a24bcb68d
Daniel Worthington-Bodart <dan@bodar.com>
2013-09-22 18:38:10 +0100

    Fixed pack support and stopped double gzip decoration

--- src/com/googlecode/shavenmaven/UnGZipHandler.java
+++ src/com/googlecode/shavenmaven/UnGZipHandler.java
@@ -6,6 +6,8 @@ import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
 import com.googlecode.utterlyidle.handlers.HttpClient;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 
 import static com.googlecode.utterlyidle.ResponseBuilder.modify;
@@ -21,8 +23,13 @@ public class UnGZipHandler implements HttpClient {
     public Response handle(final Request request) throws Exception {
         Response response = handler.handle(request);
         if("gzip".equalsIgnoreCase(response.headers().getValue(HttpHeaders.CONTENT_ENCODING))) {
-            return modify(response).entity(new GZIPInputStream(response.entity().inputStream())).removeHeaders(HttpHeaders.CONTENT_ENCODING).build();
+            return modify(response).entity(gzipInputStream(response.entity().inputStream())).removeHeaders(HttpHeaders.CONTENT_ENCODING).build();
         }
         return response;
+    }
+
+    public static GZIPInputStream gzipInputStream(InputStream inputStream) throws IOException {
+        if(inputStream instanceof GZIPInputStream) return (GZIPInputStream) inputStream;
+        return new GZIPInputStream(inputStream);
     }
 }
